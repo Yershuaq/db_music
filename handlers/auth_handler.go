a9d31ba9ccd311d80c5a0f0b70baf4a3f2114f29
solta <230930@astanait.edu.kz>
2025-01-29 01:06:45 +0500

    Second commit

--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -1,1 +1,53 @@
 package handlers
+
+import (
+	"music-streaming/database"
+	"music-streaming/models"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var jwtSecret = []byte("your_secret_key")
+
+func Register(c echo.Context) error {
+	user := new(models.User)
+	if err := c.Bind(user); err != nil {
+		return err
+	}
+
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	_, err := database.DB.Exec("INSERT INTO users (email, password) VALUES ($1, $2)", user.Email, string(hashedPassword))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": "Ошибка при регистрации"})
+	}
+
+	return c.JSON(http.StatusCreated, map[string]string{"message": "Пользователь зарегистрирован"})
+}
+
+func Login(c echo.Context) error {
+	user := new(models.User)
+	if err := c.Bind(user); err != nil {
+		return err
+	}
+
+	var hashedPassword string
+	err := database.DB.QueryRow("SELECT password FROM users WHERE email=$1", user.Email).Scan(&hashedPassword)
+	if err != nil || bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(user.Password)) != nil {
+		return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Неверный email или пароль"})
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": user.Email,
+		"exp":   time.Now().Add(72 * time.Hour).Unix(),
+	})
+	t, err := token.SignedString(jwtSecret)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{"token": t})
+}
--- utils/hash.go
+++ utils/hash.go
@@ -1,1 +1,14 @@
 package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+// HashPassword - хеширование пароля
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hashedPassword), err
+}
+
+// CheckPassword - проверка пароля
+func CheckPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
